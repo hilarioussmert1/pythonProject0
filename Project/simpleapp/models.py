d1aaec6453894216f7f9ba075ee00dc2b5e6b0dc
hilarioussmert1 <148599193+hilarioussmert1@users.noreply.github.com>
2023-12-20 01:10:58 +1100

    Update models.py

--- Project/simpleapp/models.py
+++ Project/simpleapp/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 from django.core.validators import MinValueValidator
+from django.urls import reverse
+
+
+class Material(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
+    skin = 'кожа'
+    volokno = 'волокно'
+    CATEGORY = [(skin, 'кожа'), (volokno, 'волокно')]
+
     name = models.CharField(
         max_length=50,
         unique=True,
@@ -21,8 +33,18 @@ class Product(models.Model):
         validators=[MinValueValidator(0.0)],
     )
 
+    product_material = models.ManyToManyField(Material, through='ProductMaterial')
+    choice = models.CharField(max_length=20, choices=CATEGORY, default=skin)
+
     def __str__(self):
         return f'{self.name.title()}: {self.description[:20]}'
+
+
+
+class ProductMaterial(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    material = models.ForeignKey(Material, on_delete=models.CASCADE)
+
 
 
 class Category(models.Model):
@@ -30,7 +52,6 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name.title()
-
 
 
 
